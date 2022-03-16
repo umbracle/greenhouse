6b89de0d74f3d681915daa88bed7d9214407239f
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-16 22:37:24 +0100

    Add verbosity to test

--- internal/cli/test.go
+++ internal/cli/test.go
@@ -3,18 +3,24 @@ package cli
 import (
 	"fmt"
 	"strings"
+
+	flag "github.com/spf13/pflag"
 )
 
 // TestCommand is the command to test the Solidity project
 type TestCommand struct {
 	*baseCommand
+
+	verbose bool
 }
 
 // Help implements the cli.Command interface
 func (b *TestCommand) Help() string {
 	return `Usage: greenhouse test
 
-  Test the project`
+  Test the project
+
+` + b.Flags().FlagUsages()
 }
 
 // Synopsis implements the cli.Command interface
@@ -22,9 +28,17 @@ func (b *TestCommand) Synopsis() string {
 	return "Test the project"
 }
 
+func (b *TestCommand) Flags() *flag.FlagSet {
+	flags := b.baseCommand.Flags("test")
+
+	flags.BoolVarP(&b.verbose, "verbose", "v", false, "Show in stdout the output of the test")
+
+	return flags
+}
+
 // Run implements the cli.Command interface
 func (b *TestCommand) Run(args []string) int {
-	flags := b.Flags("test")
+	flags := b.Flags()
 	if err := flags.Parse(args); err != nil {
 		b.UI.Error(err.Error())
 		return 1
@@ -34,6 +48,7 @@ func (b *TestCommand) Run(args []string) int {
 		b.UI.Error(err.Error())
 		return 1
 	}
+
 	outputs, err := b.project.Test()
 	if err != nil {
 		b.UI.Error(err.Error())
@@ -48,8 +63,10 @@ func (b *TestCommand) Run(args []string) int {
 			res = "[red]failed[reset]"
 		}
 		b.UI.Output(b.Colorize().Color(fmt.Sprintf("  %s:%s:%s (%s)", o.Source, o.Contract, o.Method, res)))
-		for _, console := range o.Console {
-			b.UI.Output("[" + strings.Join(console.Val, ", ") + "]")
+		if b.verbose {
+			for _, console := range o.Console {
+				b.UI.Output("[" + strings.Join(console.Val, ", ") + "]")
+			}
 		}
 	}
 	return 0
